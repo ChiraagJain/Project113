da3e0ede80696f9fd781f435e06228a466c68000
Chiraag Jain <78903401+ChiraagJain@users.noreply.github.com>
2022-09-27 15:17:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+function preload() { 
+
+}
+
+function setup() {
+    canvas = createCanvas(840, 480);
+    canvas.position(270, 50);
+}
+
+function draw() {
+    fill(255, 255, 255);
+    rect(70, 70, 700, 350);
+
+    circle(70, 70, 100);
+    fill(37, 183, 221);
+
+    circle(770, 70, 100);
+    fill(37, 183, 221);
+
+    circle(770, 410, 100);
+    fill(37, 183, 221);
+
+    circle(70, 410, 100);
+    fill(37, 183, 221);
+
+
+}
+
