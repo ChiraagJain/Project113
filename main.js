5c88ff0e8dcd80b09c6bc4d320e882b344577a38
Chiraag Jain <78903401+ChiraagJain@users.noreply.github.com>
2022-09-28 15:18:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,26 +3,34 @@ function preload() {
 }
 
 function setup() {
-    canvas = createCanvas(840, 480);
-    canvas.position(270, 50);
+    canvas = createCanvas(640, 480);
+    canvas.position(150, 150);
+    video = createCapture(VIDEO);
+    video.hide();
 }
 
 function draw() {
-    fill(255, 255, 255);
-    rect(70, 70, 700, 350);
+    image(video,230,150,220,200);
 
-    circle(70, 70, 100);
+    fill(146,147,241);
+    stroke(255,213,031);
+    
+
+    circle(50,50,80);
+    circle(50,430,80);
+    circle(590,50,80);
+    circle(590,430,80);
     fill(37, 183, 221);
 
-    circle(770, 70, 100);
-    fill(37, 183, 221);
+   stroke(255,213,031),
 
-    circle(770, 410, 100);
-    fill(37, 183, 221);
+   rect(90,40,460,20);
+   rect(90,420,460,20);
+   rect(40,90,20,300);
+   rect(580,90,20,300);
+}
 
-    circle(70, 410, 100);
-    fill(37, 183, 221);
-
-
+function take_snapshot(){
+    save('picture.png')
 }
 
